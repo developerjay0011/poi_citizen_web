a7cd5d555344f7ce0f20a1e98e61ae1022ceba38
Chhotala Manish <chhotalamanish7@gmail.com>
2024-02-10 17:05:07 +0530

    Remove metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,7 @@
 import { FC } from 'react'
 import { LoginForm } from '@/components/login_register/LoginForm'
 import { LoginLayout } from '@/layouts/LoginLayout'
-import { Metadata } from 'next'
 
-export const metadata: Metadata = {
-  title: 'POI | Citizens Login',
-  description: 'Explore Politician of india by logging in your account',
-}
 
 const LoginPage: FC = () => {
   return (
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,13 +1,6 @@
 'use client'
 import { RegisterForm } from '@/components/login_register/RegisterForm'
 import { LoginLayout } from '@/layouts/LoginLayout'
-import { Metadata } from 'next'
-
-export const metadata: Metadata = {
-  title: 'POI | Sign Up as Citizen',
-  description:
-    'Create an account on POI to connect with leader and emerging leaders in India',
-}
 
 const RegisterPage = () => {
   return (
