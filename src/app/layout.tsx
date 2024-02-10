2e1f0a7633bb7f86e830967286ead46669e15a29
Chhotala Manish <chhotalamanish7@gmail.com>
2024-02-10 18:51:08 +0530

    Remove Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import './globals.scss'
 import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+// import { Open_Sans } from 'next/font/google'
 import { CusProvider } from '@/redux_store/CusProvider' // Provider for Managing state using REDUX
 import { FC, ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 
-const font = Open_Sans({
+/* const font = Open_Sans({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700', '800'],
-})
+}) */
 
 export const metadata: Metadata = {
   title: 'Politician Of India | Home',
@@ -18,7 +18,8 @@ export const metadata: Metadata = {
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html lang='en'>
-      <body className={font.className + ' font-normal '}>
+      {/* <body className={font.className + ' font-normal '}> */}
+      <body >
         <div>
           <Toaster />
         </div>
